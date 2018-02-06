e14bccec5d74035086a2c4174a9bfdaf3ee390cb
chenzehua <815054699@qq.com>
2018-02-06 16:42:06 +0800

    生成新的.html文件: html-webpack-plugin 模块,1、自动添加link/script引入打包的资源文件；2、处理html模板(使用ejs语法。注：如果需要额外引入其他.html模板，需要 ejs-compiled-loader 模块并显式使用)  npm install --save-dev html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,9 @@
 * */
 const Path = require('path');
 //编译文件存放目录
-let dist = Path.join(__dirname,'dist');
-
+let path_dist = Path.join(__dirname,'dist');
+//生产开发代码
+let path_src = Path.join(__dirname,'src');
 /*
 * extract-text-webpack-plugin 模块
 * 抽取css打包
@@ -18,15 +19,29 @@ let cssIndependent = new ExtractTextWebpackPlugin({
 /*
 * postcss-loader 模块  + autoprefixer 模块
 * css 前缀自动补全
-* 额外配置文件 postcss.config.js
+* 额外配置文件 postcss.config.js （此配置文件无需手动require）
 * */
 
+/*
+*  html-webpack-plugin 模块
+*  输出新的 .html 文件
+*  1、自动添加 link/script 引入打包的资源文件
+*  2、处理html模板 ( 需要 ejs-compiled-loader 模块 )
+* */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//实例处理 src/index.html
+let htmlIndex = new HtmlWebpackPlugin({
+    template: Path.join(path_src,'index.html') //待处理的.html模板路径
+    ,title:'html-webpack-plugin 新生成的html文件' //传递给.html的参数title，通过 <%= htmlWebpackPlugin.options.title %> 调用
+    ,testData:[1,2,3,'haha'] //测试是否能传递自定义数据给.html模板 （证明是可以的）
+    ,showErrors:true
+});
 module.exports = {
     entry: {
         index: './src/index.js'
     }
     , output: {
-        path:dist,
+        path:path_dist,
         filename: 'js/[name].js'
     }
     , module: {
@@ -65,6 +80,7 @@ module.exports = {
         ]
     }
     ,plugins:[
-        cssIndependent
+        cssIndependent //css抽取打包
+        ,htmlIndex //处理生成新的.html
     ]
 }
